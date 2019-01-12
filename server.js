cb52433b7579549e455d9eb57331cca0890bdb39
Mikko Nummelin <mnummeli@gmail.com>
2019-01-12 22:55:51 +0200

    NPM installed bootstrap.

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Esimerkkisovellus vastaa portissa ${PORT}`);
